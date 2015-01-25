b22f3641e9a6e4533b50ce13b1eb6ed202bb607d
Jochen Hiller <jo.hiller@googlemail.com>
2015-01-25 22:17:43 +0100

    Updates
    Signed-off-by: Jochen Hiller <jo.hiller@googlemail.com>

--- patches/openhab2-minimal-runtime/patches/start_concierge_debug.sh
+++ patches/openhab2-minimal-runtime/patches/start_concierge_debug.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
-cd `dirname $0`
+# pipe all output to console.log file
+rm -f ./userdata/logs/console.log
+(
 
+cd `dirname $0`
 
 # ENABLE that for development purposes, will speed up
 if [ true == true ] ; then
-  cp -R ../../concierge-tests/patches/openhab2-minimal/patches/* .
+  rm -rf ./runtime/server/concierge/
+  cp -R ../../concierge-tests/patches/openhab2-minimal-runtime/patches/* .
   rm -rf ./userdata/storage
-  rm -f ./userdata/logs/*.log
+  rm -f ./userdata/logs/openhab.log
+  rm -f ./userdata/logs/events.log
 fi
+
 
 # will download all needed files if not existing
 if [ ! -d runtime/server/concierge ] ; then
   mkdir -p runtime/server/concierge
-  for f in com.sun.jersey_1.17.0.v20130314-2020.jar jetty-osgi-boot-9.2.1.v20140609.jar openhab2.xargs org.apache.felix.fileinstall-3.4.0.jar org.eclipse.concierge-5.0.0-SNAPSHOT-patched.jar org.eclipse.concierge.extension.permission_1.0.0.201407201043.jar org.eclipse.concierge.service.xmlparser_1.0.0.201407191653.jar org.eclipse.equinox.console_1.1.0.v20140131-1639.jar org.eclipse.equinox.supplement_1.5.100.v20140428-1446.jar shell-1.0.0.jar ; do
+  for f in x ; do
     curl -o runtime/server/concierge/$f https://raw.githubusercontent.com/JochenHiller/concierge-tests/master/patches/openhab2-minimal/patches/runtime/server/concierge/$f
   done
 fi
@@ -44,5 +50,6 @@ if [ ! -d userdata/logs ] ; then mkdir -p userdata/logs ; fi
 # TODO remove eclipse.ignoreApp for Concierge
 # dump complete output into log files
 set -x
-java $debug_opts $prog_args -Dosgi.clean=true -Declipse.ignoreApp=true -Dosgi.noShutdown=true -Djetty.home.bundle=org.openhab.io.jetty -Djetty.port=$HTTP_PORT -Djetty.port.ssl=$HTTPS_PORT -Dfelix.fileinstall.dir=addons -Djava.library.path=lib -Dequinox.ds.block_timeout=240000 -Dequinox.scr.waitTimeOnBlock=60000 -Dfelix.fileinstall.active.level=4 -Djava.awt.headless=true -jar $cp ./runtime/server/concierge/openhab2.xargs \
-2>&1 | tee ./userdata/logs/console.log
+java $debug_opts $prog_args -Dosgi.clean=true -Declipse.ignoreApp=true -Dosgi.noShutdown=true -Djetty.home.bundle=org.openhab.io.jetty -Djetty.port=$HTTP_PORT -Djetty.port.ssl=$HTTPS_PORT -Dfelix.fileinstall.dir=addons -Djava.library.path=lib -Dequinox.ds.block_timeout=240000 -Dequinox.scr.waitTimeOnBlock=60000 -Dfelix.fileinstall.active.level=4 -Djava.awt.headless=true -jar $cp ./runtime/server/concierge/openhab2.xargs
+
+) 2>&1 | tee ./userdata/logs/console.log
